2b0865bed75ffab887b72478f3e8f324221cedab
u1809 <1473925065@qq.com>
2019-05-24 10:42:44 +0800

    web

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,16 @@
 */
 
 
+Route::get('index', 'Index\IndexController@index');  //首页展示
+
+Route::get('reg', 'Login\RegController@reg');  //注册展示
+
+Route::post('register', 'Login\RegController@register');  //注册执行
+
+Route::get('log', 'Login\LogController@log');  //登录展示
+
+Route::post('login', 'Login\LogController@login');  //登录执行
+
+
 
 
