c72920582b2deef966d9e237c5f8718946cacb43
u1809 <1473925065@qq.com>
2019-05-27 15:35:29 +0800

    csrf

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -29,10 +29,7 @@ class VerifyCsrfToken extends Middleware
         'register',
         'login',
         'addcart',
-<<<<<<< HEAD
         'orderDo',
-=======
         'getcode',
->>>>>>> user
     ];
 }
