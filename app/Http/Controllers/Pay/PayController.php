7179d556c008ac58a7c6673faf1db530fff1391c
alili <2115108140@qq.com>
2019-05-27 09:16:50 +0000

    pay

--- app/Http/Controllers/Pay/PayController.php
+++ app/Http/Controllers/Pay/PayController.php
@@ -13,15 +13,13 @@ class PayController extends Controller
     public $aliPubKey;
 
 
-
-
     /**
      * 支付视图
      */
-    public function index(){
+    public function index()
+    {
         return view('pay.index');
     }
-
 
 
     public function __construct()
@@ -33,71 +31,81 @@ class PayController extends Controller
         $this->rsaPrivateKeyFilePath = storage_path('app/pay/private.key');    //应用私钥
         $this->aliPubKey = storage_path('app/pay/public.key'); //支付宝公钥
     }
+
     public function test()
     {
-        echo $this->aliPubKey;echo '</br>';
-        echo $this->rsaPrivateKeyFilePath;echo '</br>';
+        echo $this->aliPubKey;
+        echo '</br>';
+        echo $this->rsaPrivateKeyFilePath;
+        echo '</br>';
     }
+
     /**
      * 订单支付
      * @param $oid
      */
     public function pay()
     {
-        $order_id=$_GET['order_id'];
+        $order_id = $_GET['order_id'];
         //验证订单状态 是否已支付 是否是有效订单
-        $order_info = DB::table('shop_order')->where(['order_id'=>$order_id])->first();
+        $order_info = DB::table('shop_order')->where(['order_id' => $order_id])->first();
 //        echo '<pre>';print_r($order_info);echo '</pre>';echo '<hr>';
         //判断订单是否已被支付
-        if($order_info->pay_status>1){
+        if ($order_info->pay_status > 1) {
             die("订单已支付，请勿重复支付");
         }
         //业务参数
         $bizcont = [
-            'subject'           => 'Lening-Order: ' .$order_id, //order_id
-            'out_trade_no'      => $order_info->order_number,        //订单号
-            'total_amount'      => $order_info->order_amount,
-            'product_code'      => 'QUICK_WAP_WAY',
+            'subject' => 'Lening-Order: ' . $order_id, //order_id
+            'out_trade_no' => $order_info->order_number,        //订单号
+            'total_amount' => $order_info->order_amount,
+            'product_code' => 'QUICK_WAP_WAY',
         ];
         //公共参数
         $data = [
-            'app_id'   => $this->app_id,
-            'method'   => 'alipay.trade.wap.pay',
-            'format'   => 'JSON',
-            'charset'   => 'utf-8',
-            'sign_type'   => 'RSA2',
-            'timestamp'   => date('Y-m-d H:i:s'),
-            'version'   => '1.0',
-            'notify_url'   => $this->notify_url,        //异步通知地址
-            'return_url'   => $this->return_url,        // 同步通知地址
-            'biz_content'   => json_encode($bizcont),
+            'app_id' => $this->app_id,
+            'method' => 'alipay.trade.wap.pay',
+            'format' => 'JSON',
+            'charset' => 'utf-8',
+            'sign_type' => 'RSA2',
+            'timestamp' => date('Y-m-d H:i:s'),
+            'version' => '1.0',
+            'notify_url' => $this->notify_url,        //异步通知地址
+            'return_url' => $this->return_url,        // 同步通知地址
+            'biz_content' => json_encode($bizcont),
         ];
         //签名
         $sign = $this->rsaSign($data);
         $data['sign'] = $sign;
         $param_str = '?';
-        foreach($data as $k=>$v){
-            $param_str .= $k.'='.urlencode($v) . '&';
+        foreach ($data as $k => $v) {
+            $param_str .= $k . '=' . urlencode($v) . '&';
         }
-        $url = rtrim($param_str,'&');
+        $url = rtrim($param_str, '&');
         $url = $this->gate_way . $url;
-        header("Location:".$url);       // 重定向到支付宝支付页面
+        header("Location:" . $url);       // 重定向到支付宝支付页面
     }
-    public function rsaSign($params) {
+
+    public function rsaSign($params)
+    {
         return $this->sign($this->getSignContent($params));
     }
-    protected function sign($data) {
+
+    protected function sign($data)
+    {
         $priKey = file_get_contents($this->rsaPrivateKeyFilePath);
         $res = openssl_get_privatekey($priKey);
         ($res) or die('您使用的私钥格式错误，请检查RSA私钥配置');
         openssl_sign($data, $sign, $res, OPENSSL_ALGO_SHA256);
-        if(!$this->checkEmpty($this->rsaPrivateKeyFilePath)){
+        if (!$this->checkEmpty($this->rsaPrivateKeyFilePath)) {
             openssl_free_key($res);
         }
         $sign = base64_encode($sign);
         return $sign;
     }
-    public function getSignContent($params) {
+
+    public function getSignContent($params)
+    {
         ksort($params);
         $stringToBeSigned = "";
         $i = 0;
@@ -116,7 +124,9 @@ class PayController extends Controller
         unset ($k, $v);
         return $stringToBeSigned;
     }
-    protected function checkEmpty($value) {
+
+    protected function checkEmpty($value)
+    {
         if (!isset($value))
             return true;
         if ($value === null)
@@ -125,13 +135,15 @@ class PayController extends Controller
             return true;
         return false;
     }
+
     /**
      * 转换字符集编码
      * @param $data
      * @param $targetCharset
      * @return string
      */
-    function characet($data, $targetCharset) {
+    function characet($data, $targetCharset)
+    {
         if (!empty($data)) {
             $fileType = 'UTF-8';
             if (strcasecmp($fileType, $targetCharset) != 0) {
@@ -140,38 +152,41 @@ class PayController extends Controller
         }
         return $data;
     }
+
     /**
      * 支付宝异步通知
      */
     public function notify()
     {
         $pay = json_encode($_POST);
-        $log_str = "\n>>>>>> " .date('Y-m-d H:i:s') . ' '.$pay . " \n";
-        file_put_contents('/tmp/1809a_team.log',$log_str,FILE_APPEND);
-        $data=json_decode($pay,true);
-        if($data['trade_status']=='TRADE_SUCCESS'){
-            $where=[
-                'order_number'=>$data['out_trade_no'],
+        $log_str = "\n>>>>>> " . date('Y-m-d H:i:s') . ' ' . $pay . " \n";
+        file_put_contents('/tmp/1809a_team.log', $log_str, FILE_APPEND);
+        $data = json_decode($pay, true);
+        if ($data['trade_status'] == 'TRADE_SUCCESS') {
+            $where = [
+                'order_number' => $data['out_trade_no'],
             ];
-            $time=strtotime($data['notify_time']);
-            $info=[
-                'order_status'=>2,
-                'pay_type'=>1,
-                'pay_time'=>$time,
-                'utime'=>$time
+            $time = strtotime($data['notify_time']);
+            $info = [
+                'order_status' => 2,
+                'pay_type' => 1,
+                'pay_time' => $time,
+                'utime' => $time
             ];
             DB::table('shop_order')->where($where)->update($info);
-            DB::table('shop_order_detail')->where($where)->update(['detail_status'=>2]);
+            DB::table('shop_order_detail')->where($where)->update(['detail_status' => 2]);
 
-        //TODO 验签 更新订单状态
+            //TODO 验签 更新订单状态
+        }
     }
-    /**
-     * 支付宝同步通知
-     */
-    public function aliReturn()
-    {
-        echo '支付成功';
-    }
+        /**
+         * 支付宝同步通知
+         */
+        public function aliReturn()
+        {
+            echo '支付成功';
+        }
+
 }
 ?>
 
